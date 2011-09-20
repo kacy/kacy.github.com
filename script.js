df2f7b8b7261b728fae54e11041cb5b9ae1154bd
Kacy Fortner <kacyismc@manama.dreamhost.com>
2011-09-20 14:53:20 -0700

    updating with transitions

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready( function () {
+  $("#footer").animate({ top: '80%' }, 1000);
+  $("#tagline").animate({ top: '95%' }, 1000);
+  $("#terminal").animate({ "margin-left": '2%' }, 1000);
+});
