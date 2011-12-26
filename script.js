613e64c00c1aa801303d27b00650cb606ba4d2ee
Kacy Fortner <kacy@kacyfortner.com>
2011-12-25 20:47:06 -0500

    changing when the animation starts

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready( function () {
+$(window).load( function () {
   $("#footer").animate({ top: '80%' }, 1000);
   $("#tagline").animate({ top: '95%' }, 1000);
   $("#terminal").animate({ "margin-left": '2%' }, 1000);
